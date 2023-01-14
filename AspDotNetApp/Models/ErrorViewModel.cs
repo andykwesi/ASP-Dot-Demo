6d09cbacc717a31d4fb17e6dafea865db452192e
Andy Kwesi Apenteng <36931378+andykwesi@users.noreply.github.com>
2023-01-14 10:35:28 +0000

    Initial Commit

---
+++ AspDotNetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspDotNetApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
